5822bed81eaa741937bdd6f8eca67d6dd8c939ec
Roan <roanheemstra@outlook.com>
2026-04-09 17:01:32 +0200

    nog wat changes aan de html

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pico-connect-v1";
+const CACHE_NAME = "qualitylucht-pwa-v1";
 const ASSETS = [
   "./",
   "./index.html",
@@ -14,14 +14,10 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
   );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
